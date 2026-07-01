c5f3c6f9982c5107f56cd5c8089fb1604901f21d
shibe7202 <46942349+shibe7202@users.noreply.github.com>
2026-07-01 16:46:53 -0700

    Backend: define prisma models and populate db

--- blog-backend/app.js
+++ blog-backend/app.js
@@ -1,26 +1,11 @@
 import express from "express";
 import path from "node:path";
 import router from "./routes/router.js";
-import session from "express-session";
-import { prisma } from "./lib/prisma.js";
-import { PrismaSessionStore } from "@quixo3/prisma-session-store";
-import { PrismaClient } from "./generated/prisma/index.js";
+import passport from "passport";
 
 const app = express();
 
 app.use(express.json());
-
-app.use(
-    session({
-        secret: "dogs",
-        resave: false,
-        saveUninitialized: false,
-        store: new PrismaSessionStore(prisma, {
-            checkPeriod: 2 * 60 * 1000,
-            dbRecordIdIsSessionId: true,
-        }),
-    }),
-);
 
 app.use("/", router);
 
