c59b2538925d8febda6cf8bdc29c9e9746fecd44
shibe7202 <46942349+shibe7202@users.noreply.github.com>
2026-07-17 15:17:10 -0700

    Backend: Add CORS for front end api calls

--- blog-backend/app.js
+++ blog-backend/app.js
@@ -2,10 +2,12 @@ import express from "express";
 import path from "node:path";
 import router from "./routes/router.js";
 import passport from "passport";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({ origin: "http://localhost:5173" }));
 
 app.use("/api", router);
 
